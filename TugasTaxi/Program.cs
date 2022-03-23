ae14f64fd7d3d5447cf99afbf4c660ac071a9b51
raffa <raffajogja2019@gmail.com>
2022-03-23 21:16:02 +0700

    taxii

---
+++ TugasTaxi/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat objek taxi
+            Taxi taxi = new Taxi();
+
+            // penentuan nilai properties
+            taxi.DriverName = "Raffa";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 27;
+
+            //pemanggilan method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DroppOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
